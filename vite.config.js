1702fae5ccf85fc6cbdc29d2058ad60b20972214
NalenSrin123 <srinnalen@gmail.com>
2026-08-30 11:40:24 +0700

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
-
-
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
->>>>>>> e18385cbf38806269a14fa43a5d7b475a1245ce3
 })
