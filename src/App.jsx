b22a15ac75225dfcb63058e0d7080b34c7df67b7
NalenSrin123 <srinnalen@gmail.com>
2026-08-30 13:06:22 +0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
             }
             
           />
-          <Route path="/schedules" element={<ScheduleManagement />} />
+          <Route path="schedules" element={<ScheduleManagement />} />
         </Route>
       </Routes>
     </Router>
